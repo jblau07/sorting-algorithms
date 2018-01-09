46d9ca4b767fe41cce5cb4dd0fcdc1ab55aa263d
jblau07 <jlau7248@gmail.com>
2018-01-08 17:09:05 -1000

    quickSort function works

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const algorithms = require('./sortModule');
 
 const sorting = algorithms();
 
-
-const myArr = [12, 43, 5];
+const myArr = [12, 43, 5, 8, 20, 22];
 sorting.quickSort(myArr);
+console.log(sorting.quickSort(myArr));
--- sortModule.js
+++ sortModule.js
@@ -2,9 +2,23 @@ module.exports = function () {
   return {
     quickSort,
   }
-
 }
 
 function quickSort(arr) {
-  console.log(arr);
+
+  if (arr.length <= 1) {
+    return arr;
+  }
+  let pivot = arr[0];
+  let left = [];
+  let right = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] >= pivot) {
+      right.push(arr[i]);
+    } else {
+      left.push(arr[i]);
+    }
+  }
+  return quickSort(left).concat(pivot, quickSort(right));
 }
